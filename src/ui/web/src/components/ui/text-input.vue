6958ac745a9d6e40e3ecfc9801ced565fdd76cad
crucials <ensueness@gmail.com>
2024-04-02 17:37:59 +0500

    added border to the text input

--- src/ui/web/src/components/ui/text-input.vue
+++ src/ui/web/src/components/ui/text-input.vue
@@ -15,7 +15,8 @@ const emit = defineEmits<{
         :value="modelValue"
         type="text"
         class="bg-white rounded-lg text-neutral-800 transition-colors
-            flex items-center gap-x-3.5 outline-neutral-800 bg-no-repeat"
+            flex items-center gap-x-3.5 outline-neutral-800 bg-no-repeat
+            border border-neutral-300 hover:border-neutral-400"
         :class="{
             'px-4 py-3.5 sm:px-3 sm:py-3': size === 'medium',
             'px-2.5 py-1.5 sm:px-2': size === 'small'
