275bf4b35fb55248132e6d98a7612eccd224f35f
crucials <ensueness@gmail.com>
2024-04-28 19:43:30 +0500

    fixed chromium path in build release script

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -1,4 +1,4 @@
 cd ./src/ui/web
 npm run build
 cd ../../../
-python -m eel ./src/start_ui.py ./src/ui/web -y --onefile --noconsole --log-level=DEBUG --add-data="./src/ui/bin/ungoogled-chromium_120.0.6099.109-1.1.AppImage:./ui/bin" -n spoken-words-counter
+python -m eel ./src/start_ui.py ./src/ui/web -y --onefile --noconsole --log-level=DEBUG --add-data="./src/ui/bin/ungoogled-chromium.AppImage:./ui/bin" -n spoken-words-counter
