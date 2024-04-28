77eda1ba90ba66448c892ef6f787e4d7ec31d9ba
crucials <ensueness@gmail.com>
2024-04-28 11:11:05 +0500

    added windows support for ungoogled chromium loading

--- src/start_ui.py
+++ src/start_ui.py
@@ -1,6 +1,8 @@
 import sys
+import platform
 
 import eel
+import eel.browsers
 
 from utils.create_path_from_executable import create_path_from_executable
 from ui.count_from_microphone import start_counter_from_microphone, reset_counter
@@ -16,7 +18,14 @@ def stop(route, websockets):
 
 print('launching')
 
-eel.browsers.set_path('chrome', create_path_from_executable('ui', 'bin', 'ungoogled-chromium_120.0.6099.109-1.1.AppImage'))
+operating_system = platform.system()
+if operating_system == 'Linux':
+    eel.browsers.set_path('chrome', create_path_from_executable('ui', 'bin', 'ungoogled-chromium.AppImage'))
+elif operating_system == 'Windows':
+    eel.browsers.set_path('chrome', create_path_from_executable('ui', 'bin','ungoogled-chromium.exe'))
+else:
+    raise Exception(f'your os is not supported ({operating_system}), ' +
+                    'this app is available only for linux and windows')
 
 eel.init('src/ui/web/dist')
 eel.start('index.html', mode = 'chrome', size = ( 1400, 750 ), close_callback=stop)
