679af9d0ccabda9969a2b53135ee7b1c124a77f5
crucials <ensueness@gmail.com>
2024-05-12 16:42:37 +0500

    fixed build release script

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -2,4 +2,4 @@ cd ./src/ui/electron
 npm run build
 npx electron-builder
 cd ../../../
-pyinstaller ./src/start_ui.py --add-data="./src/ui/electron/dist/spoken-words-counter.AppImage:./ui/electron/dist" -y --onefile --noconsole --log-level=DEBUG -n spoken-words-counter
+pyinstaller ./src/start_ui.py --add-data="./src/ui/electron/bin/spoken-words-counter.AppImage:./ui/electron/bin" -y --onefile --noconsole --log-level=DEBUG -n spoken-words-counter
