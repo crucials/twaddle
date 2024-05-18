b5db640a2bb9bc58cb6dadc5235141a8aa198ae0
crucials <ensueness@gmail.com>
2024-05-18 14:58:35 +0500

    turned one file mode off

--- scripts/build-linux-release.sh
+++ scripts/build-linux-release.sh
@@ -3,4 +3,4 @@ cd ./src/ui/electron
 npm run build
 npx electron-builder
 cd ../../../
-pyinstaller ./src/start_ui.py --add-data="./src/ui/electron/bin/spoken-words-counter.AppImage:./ui/electron/bin" -y --onefile --noconsole --log-level=DEBUG -n spoken-words-counter
+pyinstaller ./src/start_ui.py --add-data="./src/ui/electron/bin/spoken-words-counter.AppImage:./ui/electron/bin" -y --noconsole --log-level=DEBUG -n spoken-words-counter
